ecdfdeff2481954689efe8ac60e0efe54ce8e181
ofoxxx <oleg.fokin@gmail.com>
2020-11-02 20:24:18 +0200

    HackerRank challenge
    https://www.hackerrank.com/challenges/bitset-1/problem

--- BitArray/BitArray.cpp
+++ BitArray/BitArray.cpp
@@ -1,20 +1,23 @@
-// BitArray.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <set>
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	constexpr unsigned long b = 1 << 31;
+	unsigned long N, S, P, Q;
+	cin >> N >> S >> P >> Q;
+
+	vector<unsigned long> a (N, 0);
+	a[0] = S % b;
+	for (unsigned long i = 1; i < N; ++i)
+		a[i] = (a[i - 1] * P + Q) % b;
+
+	set<unsigned long> s(a.begin(), a.end());
+	cout << s.size();
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
