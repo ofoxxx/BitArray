3f0d5e41671f87bbb4e592faf576c33091944d59
ofoxxx <oleg.fokin@gmail.com>
2020-11-06 13:40:47 +0200

    Brent's method
    https://en.wikipedia.org/wiki/Cycle_detection

--- BitArray/BitArray.cpp
+++ BitArray/BitArray.cpp
@@ -6,18 +6,52 @@
 #include <algorithm>
 using namespace std;
 
+typedef unsigned long Num;
+constexpr Num B = 1 << 31;
+
+Num A(Num a, Num P, Num Q)
+{
+	return (a*P + Q) % B;
+}
+
+Num Brent(Num N, Num S, Num P, Num Q)
+{
+	Num a0 = S % B;
+	Num m = 0, l = 1, p = 1;
+	Num t = a0, h = A(a0, P, Q);
+
+	while (t != h)
+	{
+		if (p == l)
+		{
+			t = h;
+			p *= 2;
+			l = 0;
+		}
+		h = A(h, P, Q);
+		if (++l == N)
+			return N;
+	}
+
+	t = h = a0;
+	for (Num i = 0; i < l; ++i)
+		h = A(h, P, Q);
+
+	while (t != h)
+	{
+		t = A(t, P, Q);
+		h = A(h, P, Q);
+		++m;
+	}
+	return m + l;
+}
+
 int main()
 {
-	constexpr unsigned long b = 1 << 31;
 	unsigned long N, S, P, Q;
 	cin >> N >> S >> P >> Q;
 
-	vector<unsigned long> a (N, 0);
-	a[0] = S % b;
-	for (unsigned long i = 1; i < N; ++i)
-		a[i] = (a[i - 1] * P + Q) % b;
+	cout << Brent(N, S, P, Q);
 
-	set<unsigned long> s(a.begin(), a.end());
-	cout << s.size();
 	return 0;
 }
